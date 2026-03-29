d34082d678796ac058f70679997005d2a6cf2c68
cou-kfe <cou.kfe@gmail.com>
2026-03-29 22:32:53 +0900

    Update Programs.cs

--- Programs.cs
+++ Programs.cs
@@ -7,7 +7,7 @@ using System.Xml.Linq;
 
 class Program
 {
-    static string rssUrl = "https://rsshub.app/x/user/f41co_";
+    static string rssUrl = "https://nitter.net/f41co_/rss";
     static string webhookUrl = "https://canary.discord.com/api/webhooks/1487797235751059548/NAbEzqkDyoMeNbeMiAjsQXmd3-dWvuT3yqDZexDJt20G51PEjSffCkdmsuoaz8RuAXFC";
     
     static string lastId = "";
