69efa2356ea21f30b78df1234f9bdc2a34f66bda
cou-kfe <cou.kfe@gmail.com>
2026-03-29 21:53:34 +0900

    Update Programs.cs

--- Programs.cs
+++ Programs.cs
@@ -4,56 +4,58 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.IO;
 
 class Program
 {
     static string rssUrl = "https://rsshub.app/twitter/user/WW_JP_Official";
-    static string webhookUrl = "https://canary.discord.com/api/webhooks/1487747185339535550/6PHhaqc0uu7PDxzW-TaAQGTQ6gvul1urdqv_KlHQmfIn29E9FlysTSpWitpjMYtPjCiH";
-    static string lastIdFile = "last_id.txt";
+    static string webhookUrl = "ここにWebhook";
+    
+    static string lastId = "";
 
     static async Task Main()
     {
-        
+        Console.WriteLine("起動");
+
         while (true)
         {
             await CheckFeed();
             await Task.Delay(TimeSpan.FromMinutes(5));
-            try
+        }
+    }
+
+    static async Task CheckFeed()
+    {
+        try
+        {
+            using var client = new HttpClient();
+
+            var xml = await client.GetStringAsync(rssUrl);
+            var doc = XDocument.Parse(xml);
+
+            var item = doc.Descendants("item").FirstOrDefault();
+            if (item == null) return;
+
+            var link = item.Element("link")?.Value;
+            var id = link?.Split('/').Last();
+
+            if (string.IsNullOrEmpty(id)) return;
+
+            if (id != lastId)
             {
-                var client = new HttpClient();
-                var xml = await client.GetStringAsync(rssUrl);
-    
-                var doc = XDocument.Parse(xml);
-                var items = doc.Descendants("item");
-    
-                var latestItem = items.FirstOrDefault();
-                if (latestItem == null) return;
-    
-                var link = latestItem.Element("link")?.Value;
-                var id = link?.Split('/').Last();
-    
-                if (string.IsNullOrEmpty(id)) return;
-    
-                string lastId = File.Exists(lastIdFile) ? File.ReadAllText(lastIdFile) : "";
-    
-                if (id != lastId)
-                {
-                    Console.WriteLine("新着検知: " + link);
-    
-                    await SendToDiscord(link);
-    
-                    File.WriteAllText(lastIdFile, id);
-                }
-                else
-                {
-                    Console.WriteLine("更新なし");
-                }
+                Console.WriteLine("新着: " + link);
+
+                await SendToDiscord(link);
+
+                lastId = id;
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine("エラー: " + ex.Message);
+                Console.WriteLine("更新なし");
             }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("エラー: " + ex.Message);
         }
     }
 
